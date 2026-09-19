01e31b43756b618493215e7a53c22b7335257342
ariefupgriss3-eng <ariefupgriss3@gmail.com>
2026-09-19 19:19:51 +0700

    Bump coordinator service aggregate workflow cache

--- web/finalize-html.mjs
+++ web/finalize-html.mjs
@@ -96,7 +96,7 @@ const modules=[
  ['staff-service-roles.js',1],
  ['team-workflow-authority.js',1],
  ['leadership-directions.js',1],
- ['submission-layered-workflow.js',2],
+ ['submission-layered-workflow.js',3],
  ['sekdin-role-option-fix.js',1],
  ['legacy-shell-restore.js',3],
  ['school-master-restore-fix.js',2],
